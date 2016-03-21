a0afb0aa30072e9708e3e11e2e755bbc201ed85d
ppatierno <ppatierno@live.com>
2016-03-21 18:04:57 +0100

    First commit with bridge skeleton

---
+++ src/main/java/io/ppatierno/kafka/bridge/AmqpKafkaBridge.java
@@ -0,0 +1,181 @@
+package io.ppatierno.kafka.bridge;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Vertx;
+import io.vertx.proton.ProtonConnection;
+import io.vertx.proton.ProtonReceiver;
+import io.vertx.proton.ProtonSender;
+import io.vertx.proton.ProtonServer;
+import io.vertx.proton.ProtonServerOptions;
+import io.vertx.proton.ProtonSession;
+
+/**
+ * AMQP to Kafka bridge class
+ * @author ppatierno
+ */
+public class AmqpKafkaBridge {
+
+	private static final int DEFAULT_PORT = 5672;
+	private static final String DEFAULT_HOST = "localhost";
+	
+	private static final Logger LOG = LoggerFactory.getLogger(AmqpKafkaBridge.class);
+	
+	private Vertx vertx;
+	private ProtonServer server;
+	private int port;
+	private String host;
+	
+	
+	// TODO : just for testing, another class for launching bridge ?
+	public static void main(String[] args) {
+		
+		Vertx vertx = Vertx.vertx();
+		
+		AmqpKafkaBridge bridge = new AmqpKafkaBridge(vertx);
+		bridge.start();
+		
+		try {
+			System.in.read();
+			bridge.stop();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Constructor
+	 * @param vertx		Vertx instance used to run the Proton server
+	 */
+	public AmqpKafkaBridge(Vertx vertx) {
+		
+		if (vertx == null) {
+			throw new NullPointerException("Vertx instance cannot be null");
+		}
+		this.vertx = vertx;
+		
+		this.host = DEFAULT_HOST;
+		this.port = DEFAULT_PORT;
+	}
+	
+	/**
+	 * Start the bridge
+	 */
+	public void start() {
+		
+		ProtonServerOptions options = this.createServerOptions();
+		
+		this.server = ProtonServer.create(this.vertx, options)
+				.connectHandler(this::processConnection)
+				.listen(ar -> {
+					
+					if (ar.succeeded()) {
+						LOG.info("AmqpKafkaBridge is listening on port {}", ar.result().actualPort());
+					} else {
+						LOG.error("Error starting AmqpKafkaBridge", ar.cause());
+					}
+				});
+		
+	}
+	
+	/**
+	 * Stop the bridge
+	 */
+	public void stop() {
+		
+		if (this.server != null) {
+			this.server.actualPort();
+		}
+	}
+	
+	/**
+	 * Create an options instance for the ProtonServer
+	 * based on AMQP-Kafka bridge internal configuration
+	 * 
+	 * @return		ProtonServer options instance
+	 */
+	private ProtonServerOptions createServerOptions(){
+		
+		ProtonServerOptions options = new ProtonServerOptions();
+		options.setHost(this.host);
+		options.setPort(this.port);
+		return options;
+	}
+	
+	/**
+	 * Process a connection request accepted by the Proton server
+	 * 
+	 * @param connection		Proton connection accepted instance
+	 */
+	private void processConnection(ProtonConnection connection) {
+		
+		connection
+		.openHandler(this::processOpenConnection)
+		.closeHandler(this::processCloseConnection)
+		.disconnectHandler(this::processDisconnection)
+		.sessionOpenHandler(this::processOpenSession)
+		.receiverOpenHandler(this::processOpenReceiver)
+		.senderOpenHandler(this::processOpenSender)
+		.open();
+	}
+	
+	/**
+	 * Handler for connection opened by remote
+	 * @param ar		async result with info on related Proton connection
+	 */
+	private void processOpenConnection(AsyncResult<ProtonConnection> ar) {
+		if (ar.succeeded()) {
+			LOG.info("Connection opened by {} {}", ar.result().getRemoteHostname(), ar.result().getRemoteContainer());
+		}
+	}
+	
+	/**
+	 * Handler for connection closed by remote
+	 * @param ar		async result with info on related Proton connection
+	 */
+	private void processCloseConnection(AsyncResult<ProtonConnection> ar) {
+		if (ar.succeeded()) {
+			LOG.info("Connection closed by {} {}", ar.result().getRemoteHostname(), ar.result().getRemoteContainer());
+			ar.result().close();
+		}
+	}
+	
+	/**
+	 * Handler for disconnection from the remote
+	 * @param connection	related Proton connection closed
+	 */
+	private void processDisconnection(ProtonConnection connection) {
+		LOG.info("Disconnection from {} {}", connection.getRemoteHostname(), connection.getRemoteContainer());
+	}
+	
+	/**
+	 * Handler for session opened by remote
+	 * @param session		related Proton session object
+	 */
+	private void processOpenSession(ProtonSession session) {
+		LOG.info("Session opened");
+		session.open();
+	}
+	
+	/**
+	 * Handler for attached link by a remote sender
+	 * @param receiver		receiver link created by the underlying Proton library
+	 * 						by which handling communication with remote sender
+	 */
+	private void processOpenReceiver(ProtonReceiver receiver) {
+		LOG.info("Remote sender attached");
+	}
+	
+	/**
+	 * Handler for attached link by a remote receiver
+	 * @param sender		sende link created by the underlying Proton library
+	 * 						by which handling communication with remote receiver
+	 */
+	private void processOpenSender(ProtonSender sender) {
+		LOG.info("Remote receiver attached");
+	}
+}
