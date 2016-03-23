5909e590701bd5dd81319e39d51942f12fdb5a1a
ppatierno <ppatierno@live.com>
2016-03-23 14:26:27 +0100

    1. Moved all Kafka record creation (from AMQP message) into the DefaultMessageConverter #2

--- src/main/java/io/ppatierno/kafka/bridge/DefaultMessageConverter.java
+++ src/main/java/io/ppatierno/kafka/bridge/DefaultMessageConverter.java
@@ -1,6 +1,14 @@
 package io.ppatierno.kafka.bridge;
 
+import java.util.List;
+
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.qpid.proton.amqp.Binary;
+import org.apache.qpid.proton.amqp.Symbol;
+import org.apache.qpid.proton.amqp.messaging.AmqpValue;
+import org.apache.qpid.proton.amqp.messaging.Data;
+import org.apache.qpid.proton.amqp.messaging.MessageAnnotations;
+import org.apache.qpid.proton.amqp.messaging.Section;
 import org.apache.qpid.proton.message.Message;
 
 /**
@@ -13,13 +21,63 @@ public class DefaultMessageConverter implements MessageConverter<String, byte[]>
 
 	@Override
 	public ProducerRecord<String, byte[]> toKafkaRecord(Message message) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Object partition = null, key = null;
+		byte[] value = null;
+		
+		// get topic and body from AMQP message
+		String topic = message.getAddress();
+		Section body = message.getBody();
+		
+		// check body null
+		if (body != null) {
+			
+			// section is AMQP value
+			if (body instanceof AmqpValue) {	
+				
+				Object amqpValue = ((AmqpValue) body).getValue();
+				
+				// encoded as String
+				if (amqpValue instanceof String) {
+					String content = (String)((AmqpValue) body).getValue();
+					value = content.getBytes();
+				// encoded as a List
+				} else if (amqpValue instanceof List) {
+					List<?> list = (List<?>)((AmqpValue) body).getValue();
+					value = list.toString().getBytes();
+				}
+			
+			// section is Data (binary)
+			} else if (body instanceof Data) {
+				Binary binary = (Binary)((Data)body).getValue();
+				value = binary.getArray();
+			}
+		}
+		
+		// get partition and key from AMQP message annotations
+		// NOTE : they are not mandatory
+		MessageAnnotations messageAnnotations = message.getMessageAnnotations();
+		
+		if (messageAnnotations != null) {
+			
+			partition = messageAnnotations.getValue().get(Symbol.getSymbol("x-opt-bridge.partition"));
+			key = messageAnnotations.getValue().get(Symbol.getSymbol("x-opt-bridge.key"));
+			
+			if (partition != null && !(partition instanceof Integer))
+				throw new IllegalArgumentException("The x-opt-bridge.partition annotation must be an Integer");
+			
+			if (key != null && !(key instanceof String))
+				throw new IllegalArgumentException("The x-opt-bridge.key annotation must be a String");
+		}
+		
+		// build the record for the KafkaProducer and then send it
+		return new ProducerRecord<>(topic, (Integer)partition, (String)key, value);
 	}
 
 	@Override
 	public Message toAmqpMessage(ProducerRecord<String, byte[]> record) {
-		// TODO Auto-generated method stub
+		
+		// TODO : to develop for the OutputBridgeEndpoint
 		return null;
 	}
 
--- src/main/java/io/ppatierno/kafka/bridge/InputBridgeEndpoint.java
+++ src/main/java/io/ppatierno/kafka/bridge/InputBridgeEndpoint.java
@@ -1,19 +1,12 @@
 package io.ppatierno.kafka.bridge;
 
-import java.util.List;
 import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.qpid.proton.amqp.Binary;
-import org.apache.qpid.proton.amqp.Symbol;
 import org.apache.qpid.proton.amqp.messaging.Accepted;
-import org.apache.qpid.proton.amqp.messaging.AmqpValue;
-import org.apache.qpid.proton.amqp.messaging.Data;
-import org.apache.qpid.proton.amqp.messaging.MessageAnnotations;
 import org.apache.qpid.proton.amqp.messaging.Released;
-import org.apache.qpid.proton.amqp.messaging.Section;
 import org.apache.qpid.proton.message.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,6 +25,7 @@ public class InputBridgeEndpoint implements BridgeEndpoint {
 	
 	private static final Logger LOG = LoggerFactory.getLogger(Bridge.class);
 	
+	private MessageConverter<String, byte[]> converter;
 	private Producer<String, byte[]> producer;
 	
 	public InputBridgeEndpoint() {
@@ -42,6 +36,8 @@ public class InputBridgeEndpoint implements BridgeEndpoint {
 		props.put(BridgeConfig.VALUE_SERIALIZER, BridgeConfig.getValueSerializer());
 		
 		this.producer = new KafkaProducer<>(props);
+		
+		this.converter = new DefaultMessageConverter();
 	}
 	
 	@Override
@@ -76,58 +72,9 @@ public class InputBridgeEndpoint implements BridgeEndpoint {
 	 */
 	private void processMessage(ProtonDelivery delivery, Message message) {
 		
-		Object partition = null, key = null;
-		byte[] value = null;
+		ProducerRecord<String, byte[]> record = this.converter.toKafkaRecord(message);
 		
-		// get topic and body from AMQP message
-		String topic = message.getAddress();
-		Section body = message.getBody();
-		
-		// check body null
-		if (body != null) {
-			
-			// section is AMQP value
-			if (body instanceof AmqpValue) {	
-				
-				Object amqpValue = ((AmqpValue) body).getValue();
-				
-				// encoded as String
-				if (amqpValue instanceof String) {
-					String content = (String)((AmqpValue) body).getValue();
-					value = content.getBytes();
-				// encoded as a List
-				} else if (amqpValue instanceof List) {
-					List<?> list = (List<?>)((AmqpValue) body).getValue();
-					value = list.toString().getBytes();
-				}
-			
-			// section is Data (binary)
-			} else if (body instanceof Data) {
-				Binary binary = (Binary)((Data)body).getValue();
-				value = binary.getArray();
-			}
-		}
-		
-		// get partition and key from AMQP message annotations
-		// NOTE : they are not mandatory
-		MessageAnnotations messageAnnotations = message.getMessageAnnotations();
-		
-		if (messageAnnotations != null) {
-			
-			partition = messageAnnotations.getValue().get(Symbol.getSymbol("x-opt-bridge.partition"));
-			key = messageAnnotations.getValue().get(Symbol.getSymbol("x-opt-bridge.key"));
-			
-			if (partition != null && !(partition instanceof Integer))
-				throw new IllegalArgumentException("The x-opt-bridge.partition annotation must be an Integer");
-			
-			if (key != null && !(key instanceof String))
-				throw new IllegalArgumentException("The x-opt-bridge.key annotation must be a String");
-		}
-		
-		// build the record for the KafkaProducer and then send it
-		ProducerRecord<String, byte[]> record = new ProducerRecord<>(topic, (Integer)partition, (String)key, value);
-		
-		LOG.info("Sending to Kafka on topic {} at partition {} and key {}", topic, partition, key);
+		LOG.info("Sending to Kafka on topic {} at partition {} and key {}", record.topic(), record.partition(), record.key());
 		
 		this.producer.send(record, (metadata, exception) -> {
 			
