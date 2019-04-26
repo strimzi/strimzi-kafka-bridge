4225456f20d5afed3d8564f877398d964e7728a2
Paolo Patierno <ppatierno@live.com>
2019-04-26 13:00:49 +0200

    Commit offsets test (#159)
    * Added test for offsets commit
    
    * Disabled auto commit
    
    * Updated to Vert.x Kafka Client 3.7.1-SNAPSHOT with fix on commit offsets
    hanging
    Fixed naming of enable.auto.commit property

--- src/test/java/io/strimzi/kafka/bridge/http/HttpBridgeTest.java
+++ src/test/java/io/strimzi/kafka/bridge/http/HttpBridgeTest.java
@@ -415,7 +415,7 @@ public class HttpBridgeTest extends KafkaClusterTestBase {
         JsonObject json = new JsonObject();
         json.put("name", name);
         json.put("auto.offset.reset", "earliest");
-        json.put("auto.commit.enable", "true");
+        json.put("enable.auto.commit", "true");
         json.put("fetch.min.bytes", "100");
 
         client.post(BRIDGE_PORT, BRIDGE_HOST, "/consumers/" + groupId)
@@ -761,6 +761,144 @@ public class HttpBridgeTest extends KafkaClusterTestBase {
                     String deletionStatus = bridgeResponse.getString("status");
 
                     context.assertEquals(consumerInstanceId, name);
+                    context.assertEquals("deleted", deletionStatus);
+
+                    deleteAsync.complete();
+                });
+
+        deleteAsync.await();
+    }
+
+    @Test
+    public void commitOffset(TestContext context) {
+        String topic = "commitOffset";
+        kafkaCluster.createTopic(topic, 1, 1);
+
+        String sentBody = "Simple message";
+
+        Async send = context.async();
+        kafkaCluster.useTo().produceStrings(1, send::complete, () ->
+                new ProducerRecord<>(topic, 0, null, sentBody));
+        send.await();
+
+        Async creationAsync = context.async();
+
+        WebClient client = WebClient.create(vertx);
+
+        String name = "my-kafka-consumer";
+        String groupId = "my-group";
+
+        String baseUri = "http://" + BRIDGE_HOST + ":" + BRIDGE_PORT + "/consumers/" + groupId + "/instances/" + name;
+
+        JsonObject json = new JsonObject();
+        json.put("name", name);
+        json.put("enable.auto.commit", "false");
+
+        client.post(BRIDGE_PORT, BRIDGE_HOST, "/consumers/" + groupId)
+                .putHeader("Content-length", String.valueOf(json.toBuffer().length()))
+                .as(BodyCodec.jsonObject())
+                .sendJsonObject(json, ar -> {
+                    context.assertTrue(ar.succeeded());
+
+                    HttpResponse<JsonObject> response = ar.result();
+                    JsonObject bridgeResponse = response.body();
+                    String consumerInstanceId = bridgeResponse.getString("instance_id");
+                    String consumerBaseUri = bridgeResponse.getString("base_uri");
+                    context.assertEquals(name, consumerInstanceId);
+                    context.assertEquals(baseUri, consumerBaseUri);
+                    creationAsync.complete();
+                });
+
+        creationAsync.await();
+
+        // subscribe to a topic
+        Async subscriberAsync = context.async();
+
+        JsonObject subJson = new JsonObject();
+        subJson.put("topic", topic);
+
+        client.post(BRIDGE_PORT, BRIDGE_HOST, baseUri + "/subscription")
+                .putHeader("Content-length", String.valueOf(subJson.toBuffer().length()))
+                .as(BodyCodec.jsonObject())
+                .sendJsonObject(subJson, ar -> {
+                    context.assertTrue(ar.succeeded());
+                    context.assertEquals(204, ar.result().statusCode());
+                    subscriberAsync.complete();
+                });
+
+        subscriberAsync.await();
+
+        // consume records
+        Async consumeAsync = context.async();
+
+        client.get(BRIDGE_PORT, BRIDGE_HOST, baseUri + "/records")
+                .putHeader("timeout", String.valueOf(1000))
+                .as(BodyCodec.jsonArray())
+                .send(ar -> {
+                    context.assertTrue(ar.succeeded());
+
+                    HttpResponse<JsonArray> response = ar.result();
+                    JsonObject jsonResponse = response.body().getJsonObject(0);
+
+                    String kafkaTopic = jsonResponse.getString("topic");
+                    int kafkaPartition = jsonResponse.getInteger("partition");
+                    String key = jsonResponse.getString("key");
+                    String value = jsonResponse.getString("value");
+                    long offset = jsonResponse.getLong("offset");
+
+                    context.assertEquals(topic, kafkaTopic);
+                    context.assertEquals(sentBody, value);
+                    context.assertEquals(0L, offset);
+                    context.assertNotNull(kafkaPartition);
+                    context.assertNull(key);
+
+                    consumeAsync.complete();
+                });
+
+        consumeAsync.await();
+
+        Async commitAsync = context.async();
+
+        JsonArray offsets = new JsonArray();
+        json = new JsonObject();
+        json.put("topic", "commitOffset");
+        json.put("partition", 0);
+        json.put("offset", 1);
+        offsets.add(json);
+
+        JsonObject root = new JsonObject();
+        root.put("offsets", offsets);
+
+        client.post(BRIDGE_PORT, BRIDGE_HOST, baseUri + "/offsets")
+                .putHeader("Content-length", String.valueOf(root.toBuffer().length()))
+                .as(BodyCodec.jsonObject())
+                .sendJsonObject(root, ar -> {
+                    context.assertTrue(ar.succeeded());
+
+
+                    HttpResponse<JsonObject> response = ar.result();
+
+                    int code = response.statusCode();
+                    context.assertEquals(200, code);
+                    commitAsync.complete();
+                });
+
+        commitAsync.await();
+
+        // consumer deletion
+        Async deleteAsync = context.async();
+
+        client.delete(BRIDGE_PORT, BRIDGE_HOST, baseUri)
+                .as(BodyCodec.jsonObject())
+                .send(ar -> {
+                    context.assertTrue(ar.succeeded());
+
+                    HttpResponse<JsonObject> response = ar.result();
+                    JsonObject bridgeResponse = response.body();
+                    String consumerInstanceId = bridgeResponse.getString("instance_id");
+                    String deletionStatus = bridgeResponse.getString("status");
+
+                    context.assertEquals(name, consumerInstanceId);
                     context.assertEquals("deleted", deletionStatus);
 
                     deleteAsync.complete();
