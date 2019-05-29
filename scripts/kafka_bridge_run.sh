19989c6d129d2a41f109448d895e26ba944c2ef6
Stanislav Knot <sknot@redhat.com>
2019-05-29 13:36:47 +0200

    cleanup

--- scripts/kafka_bridge_run.sh
+++ scripts/kafka_bridge_run.sh
@@ -23,36 +23,5 @@ echo "Kafka Bridge configuration:"
 ./kafka_bridge_config_generator.sh | tee /tmp/kafka-bridge.properties
 echo ""
 
-
-# Disable Kafka's GC logging (which logs to a file)...
-export GC_LOG_ENABLED="false"
-
-if [ -z "$KAFKA_LOG4J_OPTS" ]; then
-export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:$KAFKA_HOME/custom-config/log4j.properties"
-fi
-
-# We don't need LOG_DIR because we write no log files, but setting it to a
-# directory avoids trying to create it (and logging a permission denied error)
-export LOG_DIR="$KAFKA_HOME"
-
-# enabling Prometheus JMX exporter as Java agent
-if [ "$KAFKA_BRIDGE_METRICS_ENABLED" = "true" ]; then
-  export KAFKA_OPTS="-javaagent:/opt/prometheus/jmx_prometheus_javaagent.jar=9404:$KAFKA_HOME/custom-config/metrics-config.yml"
-fi
-
-if [ -z "$KAFKA_HEAP_OPTS" -a -n "${DYNAMIC_HEAP_FRACTION}" ]; then
-    . ./dynamic_resources.sh
-    # Calculate a max heap size based some DYNAMIC_HEAP_FRACTION of the heap
-    # available to a jvm using 100% of the GCroup-aware memory
-    # up to some optional DYNAMIC_HEAP_MAX
-    CALC_MAX_HEAP=$(get_heap_size ${DYNAMIC_HEAP_FRACTION} ${DYNAMIC_HEAP_MAX})
-    if [ -n "$CALC_MAX_HEAP" ]; then
-      export KAFKA_HEAP_OPTS="-Xms${CALC_MAX_HEAP} -Xmx${CALC_MAX_HEAP}"
-    fi
-fi
-
-
-. ./set_kafka_gc_options.sh
-
 # starting Kafka Bridge with final configuration
 exec java -Dvertx.cacheDirBase=/tmp -jar /kafka-bridge.jar --config-file=/tmp/kafka-bridge.properties
