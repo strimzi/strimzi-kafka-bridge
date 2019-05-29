f66417dd6f12d50d9b45441fbd16e437d6e66873
Stanislav Knot <sknot@redhat.com>
2019-05-29 15:58:07 +0200

    prepare properties

--- scripts/kafka_bridge_config_generator.sh
+++ scripts/kafka_bridge_config_generator.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Write the config file
+
 SECURITY_PROTOCOL=PLAINTEXT
 
 if [ "$KAFKA_BRIDGE_TLS" = "true" ]; then
@@ -8,18 +10,18 @@ if [ "$KAFKA_BRIDGE_TLS" = "true" ]; then
     if [ -n "$KAFKA_BRIDGE_TRUSTED_CERTS" ]; then
         TLS_CONFIGURATION=$(cat <<EOF
 # TLS / SSL
-ssl.truststore.location=/tmp/kafka/bridge.truststore.p12
-ssl.truststore.password=${CERTS_STORE_PASSWORD}
-ssl.truststore.type=PKCS12
+kafka.ssl.truststore.location=/tmp/kafka/bridge.truststore.p12
+kafka.ssl.truststore.password=${CERTS_STORE_PASSWORD}
+kafka.ssl.truststore.type=PKCS12
 EOF
 )
     fi
 
-    if [ -n "$KAFKA_BRIDGE_TLS_AUTH_CERT" ]; then
+    if [ -n "$KAFKA_BRIDGE_TLS_AUTH_CERT" ] && [ -n "$KAFKA_BRIDGE_TLS_AUTH_KEY" ]; then
         TLS_AUTH_CONFIGURATION=$(cat <<EOF
-ssl.keystore.location=/tmp/kafka/bridge.keystore.p12
-ssl.keystore.password=${CERTS_STORE_PASSWORD}
-ssl.keystore.type=PKCS12
+kafka.ssl.keystore.location=/tmp/kafka/bridge.keystore.p12
+kafka.ssl.keystore.password=${CERTS_STORE_PASSWORD}
+kafka.ssl.keystore.type=PKCS12
 EOF
 )
     fi
@@ -32,7 +34,7 @@ if [ -n "$KAFKA_BRIDGE_SASL_USERNAME" ] && [ -n "$KAFKA_BRIDGE_SASL_PASSWORD_FIL
         SECURITY_PROTOCOL="SASL_PLAINTEXT"
     fi
 
-    PASSWORD=$(cat /opt/kafka/bridge-password/$KAFKA_BRIDGE_SASL_PASSWORD_FILE)
+    PASSWORD=$(cat /opt/bridge/bridge-password/$KAFKA_BRIDGE_SASL_PASSWORD_FILE)
 
     if [ "x$KAFKA_BRIDGE_SASL_MECHANISM" = "xplain" ]; then
         SASL_MECHANISM="PLAIN"
@@ -43,41 +45,56 @@ if [ -n "$KAFKA_BRIDGE_SASL_USERNAME" ] && [ -n "$KAFKA_BRIDGE_SASL_PASSWORD_FIL
     fi
 
     SASL_AUTH_CONFIGURATION=$(cat <<EOF
-sasl.mechanism=${SASL_MECHANISM}
-sasl.jaas.config=org.apache.kafka.common.security.${JAAS_SECURITY_MODULE} required username="${KAFKA_BRIDGE_SASL_USERNAME}" password="${PASSWORD}";
+kafka.sasl.mechanism=${SASL_MECHANISM}
+kafka.sasl.jaas.config=org.apache.kafka.common.security.${JAAS_SECURITY_MODULE} required username="${KAFKA_BRIDGE_SASL_USERNAME}" password="${PASSWORD}";
 EOF
 )
 fi
 
-# Write the config file
-cat <<EOF
-#Apache Kafka common
-kafka.bootstrapServers=${KAFKA_BRIDGE_BOOTSTRAP_SERVERS}
+content="#Common properties\n"
+content="${content}kafka.security.protocol=${SECURITY_PROTOCOL}"
+content="${content}${TLS_CONFIGURATION}"
+content="${content}${TLS_AUTH_CONFIGURATION}"
+content="${content}${SASL_AUTH_CONFIGURATION}"
+content="$content\n"
+content="${content}kafka.bootstrap.servers=${KAFKA_BRIDGE_BOOTSTRAP_SERVERS}\n"
 
-#Apache Kafka producer
-kafka.producer.acks=${KAFKA_BRIDGE_PRODUCER_ACKS}
+content="$content\n"
 
-#Apache Kafka consumer
-kafka.consumer.autoOffsetReset=${KAFKA_BRIDGE_CONSUMER_AUTO_OFFSET_RESET}
+content="${content}#Apache Kafka Producer\n"
 
-#AMQP related settings
-amqp.enabled=${KAFKA_BRIDGE_AMQP_ENABLED}
-amqp.flowCredit=${KAFKA_BRIDGE_AMQP_FLOW_CREDIT}
-amqp.mode=${KAFKA_BRIDGE_AMQP_MODE}
-amqp.host=${KAFKA_BRIDGE_AMQP_HOST}
-amqp.port=${KAFKA_BRIDGE_AMQP_PORT}
-amqp.certDir=${KAFKA_BRIDGE_AMQP_CERT_DIR}
-amqp.messageConverter=${KAFKA_BRIDGE_AMQP_MESSAGE_CONNVERTER}
+for i in $KAFKA_BRIDGE_PRODUCER_CONFIG; do
+	content="${content}kafka.producer."
+	content="${content}$(echo -n $i | cut -d'=' -f1)"
+	content="$content="
+	content="$content$(echo -n $i | cut -d'=' -f2)"
+	content="$content\n"
+done
 
-#HTTP related settings
-http.enabled=${KAFKA_BRIDGE_HTTP_ENABLED}
-http.host=${KAFKA_BRIDGE_HTTP_HOST}
-http.port=${KAFKA_BRIDGE_HTTP_PORT}
+content="$content\n"
+content="$content#Apache Kafka Consumer\n"
+for i in $KAFKA_BRIDGE_CONSUMER_CONFIG; do
+	content="${content}kafka.consumer."
+	content="$content$(echo -n $i | cut -d'=' -f1)"
+	content="$content="
+	content="$content$(echo -n $i | cut -d'=' -f2)"
+	content="$content\n"
+done
 
-security.protocol=${SECURITY_PROTOCOL}
-${TLS_CONFIGURATION}
-${TLS_AUTH_CONFIGURATION}
-${SASL_AUTH_CONFIGURATION}
+content="$content\n"
+content="$content#HTTP\n"
+content="${content}http.enabled=$KAFKA_BRIDGE_HTTP_ENABLED\n"
+content="${content}http.host=$KAFKA_BRIDGE_HTTP_HOST\n"
+content="${content}http.port=$KAFKA_BRIDGE_HTTP_PORT\n"
 
+content="$content\n"
+content="$content#AMQP\n"
+content="${content}amqp.enabled=$KAFKA_BRIDGE_AMQP_ENABLED\n"
+content="${content}amqp.host=$KAFKA_BRIDGE_AMQP_HOST\n"
+content="${content}amqp.port=$KAFKA_BRIDGE_AMQP_PORT\n"
+content="${content}amqp.mode=$KAFKA_BRIDGE_AMQP_MODE\n"
+content="${content}amqp.flowCredit=$KAFKA_BRIDGE_AMQP_FLOW_CREDIT\n"
+content="${content}amqp.certDir=$KAFKA_BRIDGE_AMQP_CERT_DIR\n"
+content="${content}amqp.messageConverter=$KAFKA_BRIDGE_AMQP_MESSAGE_CONVERTER\n"
 
-EOF
+echo -e $content
