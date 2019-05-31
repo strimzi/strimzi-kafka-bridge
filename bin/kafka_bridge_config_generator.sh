b7c6ca434397886c286c580d0d03d70cb62c422a
Stanislav Knot <sknot@redhat.com>
2019-05-31 14:11:05 +0200

    improved script

--- bin/kafka_bridge_config_generator.sh
+++ bin/kafka_bridge_config_generator.sh
@@ -7,7 +7,7 @@ if [ "$KAFKA_BRIDGE_TLS" = "true" ]; then
 
     if [ -n "$KAFKA_BRIDGE_TRUSTED_CERTS" ]; then
         TLS_CONFIGURATION=$(cat <<EOF
-# TLS / SSL
+#TLS/SSL
 kafka.ssl.truststore.location=/tmp/kafka/bridge.truststore.p12
 kafka.ssl.truststore.password=${CERTS_STORE_PASSWORD}
 kafka.ssl.truststore.type=PKCS12
@@ -44,55 +44,94 @@ if [ -n "$KAFKA_BRIDGE_SASL_USERNAME" ] && [ -n "$KAFKA_BRIDGE_SASL_PASSWORD_FIL
 
     SASL_AUTH_CONFIGURATION=$(cat <<EOF
 kafka.sasl.mechanism=${SASL_MECHANISM}
-kafka.sasl.jaas.config=org.apache.kafka.common.security.${JAAS_SECURITY_MODULE} required username="${KAFKA_BRIDGE_SASL_USERNAME}" password="${PASSWORD}";
+kafka.sasl.jaas.config=org.apache.kafka.common.security.${JAAS_SECURITY_MODULE}
+ required username="${KAFKA_BRIDGE_SASL_USERNAME}"
+  password="${PASSWORD}";
 EOF
 )
 fi
 
-content="#Kafka common properties\n"
-content="${content}kafka.security.protocol=${SECURITY_PROTOCOL}"
-content="${content}${TLS_CONFIGURATION}"
-content="${content}${TLS_AUTH_CONFIGURATION}"
-content="${content}${SASL_AUTH_CONFIGURATION}"
-content="$content\n"
-content="${content}kafka.bootstrap.servers=${KAFKA_BRIDGE_BOOTSTRAP_SERVERS}\n"
+KAFKA_PROPERTIES=$(cat <<-EOF
+#Kafka common properties
+kafka.bootstrap.servers=${KAFKA_BRIDGE_BOOTSTRAP_SERVERS}
+kafka.security.protocol=${SECURITY_PROTOCOL}
+${TLS_CONFIGURATION}
+${TLS_AUTH_CONFIGURATION}
+${SASL_AUTH_CONFIGURATION}
+EOF
+)
 
-content="$content\n"
-
-content="${content}#Apache Kafka Producer\n"
+PRODUCER_PROPERTIES="#Apache Kafka Producer"
 
 for i in $KAFKA_BRIDGE_PRODUCER_CONFIG; do
-	content="${content}kafka.producer."
-	content="${content}$(echo -n $i | cut -d'=' -f1)"
-	content="$content="
-	content="$content$(echo -n $i | cut -d'=' -f2)"
-	content="$content\n"
+
+        key="kafka.producer.$(echo $i | cut -d'=' -f1)"
+        value="$(echo -n $i | cut -d'=' -f2)"
+        PRODUCER_PROPERTIES=$(cat <<EOF
+$PRODUCER_PROPERTIES
+${key}=${value}
+EOF
+)
 done
 
-content="$content\n"
-content="$content#Apache Kafka Consumer\n"
+
+CONSUMER_PROPERTIES="#Apache Kafka Consumer"
 for i in $KAFKA_BRIDGE_CONSUMER_CONFIG; do
-	content="${content}kafka.consumer."
-	content="$content$(echo -n $i | cut -d'=' -f1)"
-	content="$content="
-	content="$content$(echo -n $i | cut -d'=' -f2)"
-	content="$content\n"
+        key="kafka.consumer.$(echo $i | cut -d'=' -f1)"
+        value="$(echo -n $i | cut -d'=' -f2)"
+        CONSUMER_PROPERTIES=$(cat <<EOF
+$CONSUMER_PROPERTIES
+${key}=${value}
+EOF
+)
 done
 
-content="$content\n"
-content="$content#HTTP\n"
-content="${content}http.enabled=$KAFKA_BRIDGE_HTTP_ENABLED\n"
-content="${content}http.host=$KAFKA_BRIDGE_HTTP_HOST\n"
-content="${content}http.port=$KAFKA_BRIDGE_HTTP_PORT\n"
+HTTP_PROPERTIES=$(cat <<-EOF
+#HTTP configuration
+http.enabled=${KAFKA_BRIDGE_HTTP_ENABLED}
+http.host=${KAFKA_BRIDGE_HTTP_HOST}
+http.port=${KAFKA_BRIDGE_HTTP_PORT}
+EOF
+)
+AMQP_PROPERTIES=$(cat <<-EOF
+#AMQP configuration
+amqp.enabled=${KAFKA_BRIDGE_AMQP_ENABLED}
+amqp.host=${KAFKA_BRIDGE_AMQP_HOST}
+amqp.port=${KAFKA_BRIDGE_AMQP_PORT}
+amqp.mode=${KAFKA_BRIDGE_AMQP_MODE}
+amqp.flowCredit=${KAFKA_BRIDGE_AMQP_FLOW_CREDIT}
+amqp.certDir=${KAFKA_BRIDGE_AMQP_CERT_DIR}
+amqp.messageConverter=${KAFKA_BRIDGE_AMQP_MESSAGE_CONVERTER}
+EOF
+)
 
-content="$content\n"
-content="$content#AMQP\n"
-content="${content}amqp.enabled=$KAFKA_BRIDGE_AMQP_ENABLED\n"
-content="${content}amqp.host=$KAFKA_BRIDGE_AMQP_HOST\n"
-content="${content}amqp.port=$KAFKA_BRIDGE_AMQP_PORT\n"
-content="${content}amqp.mode=$KAFKA_BRIDGE_AMQP_MODE\n"
-content="${content}amqp.flowCredit=$KAFKA_BRIDGE_AMQP_FLOW_CREDIT\n"
-content="${content}amqp.certDir=$KAFKA_BRIDGE_AMQP_CERT_DIR\n"
-content="${content}amqp.messageConverter=$KAFKA_BRIDGE_AMQP_MESSAGE_CONVERTER\n"
+# if http/amqp is disabled, do not print its configuration
+PROPERTIES=$(cat <<EOF
+$KAFKA_PROPERTIES
 
-echo -e $content
+$PRODUCER_PROPERTIES
+
+$CONSUMER_PROPERTIES
+EOF
+)
+if [[ -n "$KAFKA_BRIDGE_HTTP_ENABLED" && "$KAFKA_BRIDGE_HTTP_ENABLED" = "true" ]]; then
+	PROPERTIES=$(cat <<EOF
+$PROPERTIES
+
+$HTTP_PROPERTIES
+EOF
+)
+fi
+
+if [[ -n "$KAFKA_BRIDGE_AMQP_ENABLED" && "$KAFKA_BRIDGE_AMQP_ENABLED" = "true" ]]; then
+	PROPERTIES=$(cat <<EOF
+$PROPERTIES
+
+$AMQP_PROPERTIES
+EOF
+)
+fi
+
+cat <<EOF
+$PROPERTIES
+EOF
