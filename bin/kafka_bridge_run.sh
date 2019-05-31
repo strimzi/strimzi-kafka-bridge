d274b5ae3890be1489f018cd047925532a44c7de
Jakub Scholz <www@scholzj.com>
2019-05-31 15:30:13 +0200

    Get the scripts working

--- bin/kafka_bridge_run.sh
+++ bin/kafka_bridge_run.sh
@@ -1,27 +1,5 @@
-#!/usr/bin/env bash
-set +x
+#!/bin/sh
 
-if [ -n "$KAFKA_BRIDGE_TRUSTED_CERTS" ]; then
-    # Generate temporary keystore password
-    export CERTS_STORE_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
+MYPATH="$(dirname "$0")"
 
-    mkdir -p /tmp/kafka
-
-    # Import certificates into keystore and truststore
-    # $1 = trusted certs, $2 = TLS auth cert, $3 = TLS auth key, $4 = truststore path, $5 = keystore path, $6 = certs and key path
-    ./kafka_bridge_tls_prepare_certificates.sh \
-        "$KAFKA_BRIDGE_TRUSTED_CERTS" \
-        "$KAFKA_BRIDGE_TLS_AUTH_CERT" \
-        "$KAFKA_BRIDGE_TLS_AUTH_KEY" \
-        "/tmp/kafka/bridge.truststore.p12" \
-        "/tmp/kafka/bridge.keystore.p12" \
-        "/opt/kafka/bridge-certs"
-fi
-
-# Generate and print the consumer config file
-echo "Kafka Bridge configuration:"
-./kafka_bridge_config_generator.sh | tee /tmp/kafka-bridge.properties
-echo ""
-
-# starting Kafka Bridge with final configuration
-exec java -Dvertx.cacheDirBase=/tmp -cp "libs/*" io.strimzi.kafka.bridge.Application "$@" --config-file=/tmp/kafka-bridge.properties
+exec java -Dvertx.cacheDirBase=/tmp -cp "${MYPATH}/../libs/*" io.strimzi.kafka.bridge.Application "$@"
--- bin/run_bridge.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-exec java -Dvertx.cacheDirBase=/tmp -cp "libs/*" io.strimzi.kafka.bridge.Application "$@"
