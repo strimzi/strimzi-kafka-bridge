50da9712eb36d6a51129532287fbba135cd52abf
Jakub Scholz <www@scholzj.com>
2019-06-01 00:14:38 +0200

    Add improved configuration for logging and memory

--- bin/docker/kafka_bridge_run.sh
+++ bin/docker/kafka_bridge_run.sh
@@ -25,5 +25,36 @@ echo "Kafka Bridge configuration:"
 ${MYPATH}/kafka_bridge_config_generator.sh | tee /tmp/kafka-bridge.properties
 echo ""
 
+# Configure logging for Kubernetes deployments
+KAFKA_BRIDGE_LOG4J_OPTS="-Dlog4j.configuration=file:$STRIMZI_HOME/custom-config/log4j.properties"
+
+# Configure Memory
+. ${MYPATH}/dynamic_resources.sh
+
+MAX_HEAP=`get_heap_size`
+if [ -n "$MAX_HEAP" ]; then
+  export JAVA_OPTS="-Xms${MAX_HEAP}m -Xmx${MAX_HEAP}m $JAVA_OPTS"
+fi
+
+export MALLOC_ARENA_MAX=2
+
+# Configure GC logging for memory tracking
+function get_gc_opts {
+  if [ "${STRIMZI_GC_LOG_ENABLED}" == "true" ]; then
+    # The first segment of the version number is '1' for releases < 9; then '9', '10', '11', ...
+    JAVA_MAJOR_VERSION=$(java -version 2>&1 | sed -E -n 's/.* version "([0-9]*).*$/\1/p')
+    if [ "$JAVA_MAJOR_VERSION" -ge "9" ] ; then
+      echo "-Xlog:gc*:stdout:time -XX:NativeMemoryTracking=summary"
+    else
+      echo "-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:NativeMemoryTracking=summary"
+    fi
+  else
+    # no gc options
+    echo ""
+  fi
+}
+
+export JAVA_OPTS="${JAVA_OPTS} $(get_gc_opts)"
+
 # starting Kafka Bridge with final configuration
 ${MYPATH}/../kafka_bridge_run.sh --config-file=/tmp/kafka-bridge.properties "$@"
