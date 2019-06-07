9aff2c23904d34a2f6659e08fb4ae81208c31425
Jakub Scholz <www@scholzj.com>
2019-06-07 10:10:49 +0200

    Build API docs and push them to the website (#236)
    * Build docs and push them to the website
    
    * Add doc to the release process in HACKING.md
    
    * Add empty directory whihc is prepared for images
    
    * Install Asciidoctor

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,6 +4,9 @@ set -e
 echo "Run Findbugs ..."
 make findbugs
 
+echo "Run docu check ..."
+make docu_check
+
 echo "Packaging ..."
 make java_package
 
@@ -13,8 +16,14 @@ echo "BRANCH=$BRANCH"
 
 # Docker build and push only if on master, no pull request and "latest" tag
 if [ "$PULL_REQUEST" != "false" ] ; then
+    make docu_html
+    make docu_htmlnoheader
+
     echo "Building Pull Request - nothing to push"
 elif [ "$TAG" = "latest" ] && [ "$BRANCH" != "master" ] ; then
+    make docu_html
+    make docu_htmlnoheader
+
     echo "Not in master branch and not in release tag - nothing to push"
 else
     echo "Login into Docker Hub ..."
@@ -28,4 +37,8 @@ else
 
     echo "Pushing to Docker Hub ..."
     make docker_push
+
+    if [ "$BRANCH" = "master" ]; then
+        make docu_pushtowebsite
+    fi
 fi
---
+++ .travis/docu-push-to-website.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+openssl aes-256-cbc -K $encrypted_92c411a5688b_key -iv $encrypted_92c411a5688b_iv -in .travis/github_deploy_key.enc -out github_deploy_key -d
+
+openssl aes-256-cbc -K $encrypted_c563b2a48eea_key -iv $encrypted_c563b2a48eea_iv -in .travis/github_deploy_key.enc -out github_deploy_key -d
+chmod 600 github_deploy_key
+eval `ssh-agent -s`
+ssh-add github_deploy_key
+
+git clone git@github.com:strimzi/strimzi.github.io.git /tmp/website
+cp -v documentation/htmlnoheader/master.html /tmp/website/docs/bridge/master/master.html
+cp -v documentation/html/index.html /tmp/website/docs/bridge/master/full.html
+rm -rf /tmp/website/docs/bridge/master/images
+cp -vrL documentation/htmlnoheader/images /tmp/website/docs/bridge/master/images
+
+pushd /tmp/website
+
+if [[ -z $(git status -s) ]]; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
+
+git config user.name "Travis CI"
+git config user.email "ci@travis.tld"
+
+git add -A
+git commit -m "Update Kafka Bridge documentation (Travis CI build ${TRAVIS_BUILD_NUMBER})" --allow-empty
+git push origin master
+
+popd
