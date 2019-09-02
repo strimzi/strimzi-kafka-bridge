11b56b0be414d4a0d3b30870ab9bd325bbeb3692
Stanislav Knot <sknot@redhat.com>
2019-09-02 15:18:53 +0200

    rebase
    Signed-off-by: Stanislav Knot <sknot@redhat.com>

--- src/main/java/io/strimzi/kafka/bridge/http/HttpSourceBridgeEndpoint.java
+++ src/main/java/io/strimzi/kafka/bridge/http/HttpSourceBridgeEndpoint.java
@@ -21,7 +21,6 @@ import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
-import io.vertx.core.impl.CompositeFutureImpl;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
