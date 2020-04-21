39c71bf8ac3c24444e79b3d05d3a7fbd1bf42143
hifly81 <gio.marigi@gmail.com>
2020-04-21 10:25:51 +0200

    refactoring tests methods for cors
    Signed-off-by: hifly81 <gio.marigi@gmail.com>

--- src/test/java/io/strimzi/kafka/bridge/http/HttpCorsTests.java
+++ src/test/java/io/strimzi/kafka/bridge/http/HttpCorsTests.java
@@ -14,6 +14,7 @@ import io.vertx.core.VertxOptions;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.junit5.Checkpoint;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import org.apache.log4j.LogManager;
@@ -77,78 +78,67 @@ public class HttpCorsTests {
         kafkaCluster.stop();
     }
 
+    /**
+     * When CORS is not enabled, OPTIONS requests are not supported (method not allowed)
+     * Real requests (GET, POST, PUT, DELETE) are allowed
+     */
     @Test
     public void testCorsNotEnabled(VertxTestContext context) {
         createWebClient(false, null, context);
-
-        if (!"TRUE".equalsIgnoreCase(System.getenv("STRIMZI_USE_SYSTEM_BRIDGE"))) {
-            vertx.deployVerticle(httpBridge, context.succeeding(id -> client
-                .request(HttpMethod.OPTIONS, 8080, "localhost", "/ready")
-                .putHeader("Origin", "https://evil.io")
-                .putHeader("Access-Control-Request-Method", "GET")
-                .send(ar -> {
-                    context.verify(() -> {
-                        assertThat(ar.result().statusCode(), is(405));
-                        context.completeNow();
-                    });
-
-                })));
-
-        } else {
-            context.completeNow();
-        }
-    }
-
-    @Test
-    public void testOriginForbiddenRequest(VertxTestContext context) {
-        createWebClient(true, null, context);
+        configureBridge();
 
         if (!"TRUE".equalsIgnoreCase(System.getenv("STRIMZI_USE_SYSTEM_BRIDGE"))) {
             vertx.deployVerticle(httpBridge, context.succeeding(id -> client
                     .request(HttpMethod.OPTIONS, 8080, "localhost", "/ready")
                     .putHeader("Origin", "https://evil.io")
                     .putHeader("Access-Control-Request-Method", "GET")
-                    .send(ar -> {
-                        context.verify(() -> {
-                            assertThat(ar.result().statusCode(), is(403));
-                            context.completeNow();
-                        });
-
-                    })));
-
+                    .send(ar -> context.verify(() -> {
+                        assertThat(ar.result().statusCode(), is(405));
+                        client.request(HttpMethod.GET, 8080, "localhost", "/ready")
+                                .putHeader("Origin", "https://evil.io")
+                                .send(ar2 -> context.verify(() -> {
+                                    assertThat(ar2.result().statusCode(), is(200));
+                                    context.completeNow();
+                                }));
+                    }))));
         } else {
             context.completeNow();
         }
     }
 
+    /**
+     * When CORS is enabled, OPTIONS requests are supported
+     * Real requests (GET, POST, PUT, DELETE) for domains not trusted are not allowed
+     */
     @Test
-    public void testOriginAllowedRequest(VertxTestContext context) {
+    public void testCorsForbidden(VertxTestContext context) {
         createWebClient(true, null, context);
-        final String origin = "https://strimzi.io";
+        configureBridge();
 
         if (!"TRUE".equalsIgnoreCase(System.getenv("STRIMZI_USE_SYSTEM_BRIDGE"))) {
             vertx.deployVerticle(httpBridge, context.succeeding(id -> client
                     .request(HttpMethod.OPTIONS, 8080, "localhost", "/ready")
-                    .putHeader("Origin", "https://strimzi.io")
+                    .putHeader("Origin", "https://evil.io")
                     .putHeader("Access-Control-Request-Method", "GET")
-                    .send(ar -> {
-                        context.verify(() -> {
-                            assertThat(ar.result().statusCode(), is(200));
-                            assertThat(ar.result().getHeader("access-control-allow-origin"), is(origin));
-                            assertThat(ar.result().getHeader("access-control-allow-headers"), is("Access-Control-Allow-Origin,origin,x-requested-with,Content-Type,accept"));
-                            context.completeNow();
-                        });
-
-                    })));
+                    .send(ar -> context.verify(() -> {
+                        assertThat(ar.result().statusCode(), is(403));
+                        context.completeNow();
+                    }))));
 
         } else {
             context.completeNow();
         }
     }
 
+    /**
+     * When CORS is enabled, OPTIONS requests are supported
+     * Real requests (GET, POST, PUT, DELETE) for domains trusted are allowed
+     */
     @Test
-    public void testMethodNotAllowedRequest(VertxTestContext context) {
-        createWebClient(true, "POST,PUT,DELETE,OPTIONS,PATCH", context);
+    public void testCorsOriginAllowed(VertxTestContext context) {
+        createWebClient(true, null, context);
+        configureBridge();
+
         final String origin = "https://strimzi.io";
 
         if (!"TRUE".equalsIgnoreCase(System.getenv("STRIMZI_USE_SYSTEM_BRIDGE"))) {
@@ -156,18 +146,50 @@ public class HttpCorsTests {
                     .request(HttpMethod.OPTIONS, 8080, "localhost", "/ready")
                     .putHeader("Origin", "https://strimzi.io")
                     .putHeader("Access-Control-Request-Method", "GET")
-                    .send(ar -> {
-                        context.verify(() -> {
-                            assertThat(ar.result().statusCode(), is(200));
-                            assertThat(ar.result().getHeader("access-control-allow-origin"), is(origin));
-                            assertThat(ar.result().getHeader("access-control-allow-headers"), is("Access-Control-Allow-Origin,origin,x-requested-with,Content-Type,accept"));
-                            List<String> list = Arrays.asList(ar.result().getHeader("access-control-allow-methods").split(","));
-                            assertThat(list, not(hasItem("GET")));
-                            context.completeNow();
-                        });
+                    .send(ar -> context.verify(() -> {
+                        assertThat(ar.result().statusCode(), is(200));
+                        assertThat(ar.result().getHeader("access-control-allow-origin"), is(origin));
+                        assertThat(ar.result().getHeader("access-control-allow-headers"), is("Access-Control-Allow-Origin,origin,x-requested-with,Content-Type,accept"));
+                        List<String> list = Arrays.asList(ar.result().getHeader("access-control-allow-methods").split(","));
+                        assertThat(list, hasItem("GET"));
+                        client.request(HttpMethod.GET, 8080, "localhost", "/ready")
+                                .putHeader("Origin", "https://strimzi.io")
+                                .send(ar2 -> context.verify(() -> {
+                                    assertThat(ar2.result().statusCode(), is(200));
+                                    context.completeNow();
+                                }));
+                    }))));
+        } else {
+            context.completeNow();
+        }
+    }
 
-                    })));
+    /**
+     * When CORS is enabled, OPTIONS requests are supported
+     * Real requests (GET, POST, PUT, DELETE) for domains listed are allowed.
+     * However browser will check that the method set in Access-Control-Request-Method request header
+     * is contained in access-control-allow-methods responde header
+     */
+    @Test
+    public void testCorsMethodNotAllowed(VertxTestContext context) {
+        createWebClient(true, "POST,PUT,DELETE,OPTIONS,PATCH", context);
+        configureBridge();
 
+        final String origin = "https://strimzi.io";
+
+        if (!"TRUE".equalsIgnoreCase(System.getenv("STRIMZI_USE_SYSTEM_BRIDGE"))) {
+            vertx.deployVerticle(httpBridge, context.succeeding(id -> client
+                    .request(HttpMethod.OPTIONS, 8080, "localhost", "/ready")
+                    .putHeader("Origin", "https://strimzi.io")
+                    .putHeader("Access-Control-Request-Method", "GET")
+                    .send(ar -> context.verify(() -> {
+                        assertThat(ar.result().statusCode(), is(200));
+                        assertThat(ar.result().getHeader("access-control-allow-origin"), is(origin));
+                        assertThat(ar.result().getHeader("access-control-allow-headers"), is("Access-Control-Allow-Origin,origin,x-requested-with,Content-Type,accept"));
+                        List<String> list = Arrays.asList(ar.result().getHeader("access-control-allow-methods").split(","));
+                        assertThat(list, not(hasItem("GET")));
+                        context.completeNow();
+                    }))));
         } else {
             context.completeNow();
         }
@@ -178,16 +200,18 @@ public class HttpCorsTests {
         config.put(HttpConfig.HTTP_CORS_ALLOWED_ORIGINS, "https://strimzi.io");
         config.put(HttpConfig.HTTP_CORS_ALLOWED_METHODS, methodsAllowed != null ? methodsAllowed : "GET,POST,PUT,DELETE,OPTIONS,PATCH");
 
-        if (!"TRUE".equalsIgnoreCase(System.getenv("STRIMZI_USE_SYSTEM_BRIDGE"))) {
-            bridgeConfig = BridgeConfig.fromMap(config);
-            httpBridge = new HttpBridge(bridgeConfig);
-            httpBridge.setHealthChecker(new HealthChecker());
-        }
-
         client = WebClient.create(vertx, new WebClientOptions()
                 .setDefaultHost(Urls.BRIDGE_HOST)
                 .setDefaultPort(Urls.BRIDGE_PORT)
         );
 
+    }
+
+    private void configureBridge() {
+        if (!"TRUE".equalsIgnoreCase(System.getenv("STRIMZI_USE_SYSTEM_BRIDGE"))) {
+            bridgeConfig = BridgeConfig.fromMap(config);
+            httpBridge = new HttpBridge(bridgeConfig);
+            httpBridge.setHealthChecker(new HealthChecker());
+        }
     }
 }
