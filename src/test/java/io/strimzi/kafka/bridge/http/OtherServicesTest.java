8c6a89838ec260f221bc8db0ae5da0c586cc6145
Stanislav Knot <sknot@redhat.com>
2020-06-10 12:51:52 +0200

    bridgeInfo data type
    Signed-off-by: Stanislav Knot <sknot@redhat.com>

--- src/test/java/io/strimzi/kafka/bridge/http/OtherServicesTest.java
+++ src/test/java/io/strimzi/kafka/bridge/http/OtherServicesTest.java
@@ -113,6 +113,7 @@ public class OtherServicesTest extends HttpBridgeTestBase {
                         assertThat(bridgeResponse.getJsonObject("paths").getJsonObject("/").getJsonObject("get").getString("operationId"), is(HttpOpenApiOperations.INFO.toString()));
                         assertThat(paths.containsKey("/karel"), is(false));
                         assertThat(bridgeResponse.getJsonObject("definitions").getMap().size(), is(21));
+                        assertThat(bridgeResponse.getJsonObject("definitions").getMap().size(), is(19));
                         assertThat(bridgeResponse.getJsonArray("tags").size(), is(4));
                     });
                     context.completeNow();
