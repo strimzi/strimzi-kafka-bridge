6a947e4eb30a158f288c7870cfeae28b5466ac06
Jakub Scholz <www@scholzj.com>
2021-06-17 13:35:22 +0200

    Fix pushing to Nexus
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- .azure/scripts/push-to-nexus.sh
+++ .azure/scripts/push-to-nexus.sh
@@ -6,7 +6,7 @@ export GPG_TTY
 echo "$GPG_SIGNING_KEY" | base64 -d > signing.gpg
 gpg --batch --import signing.gpg
 
-GPG_EXECUTABLE=gpg mvn "$MVN_ARGS" -DskipTests -s ./.azure/scripts/settings.xml -P ossrh verify deploy
+GPG_EXECUTABLE=gpg mvn $MVN_ARGS -DskipTests -s ./.azure/scripts/settings.xml -P ossrh verify deploy
 
 rm -rf signing.gpg
 gpg --delete-keys
