f9eabb138b8a76ab2771ea6a577a133da8602a95
ShubhamRwt <srawat@redhat.com>
2021-08-04 22:20:44 +0530

    shift from debezium cluster to embedded kafka cluster
    Signed-off-by: ShubhamRwt <srawat@redhat.com>

--- src/test/java/io/strimzi/kafka/bridge/amqp/AmqpBridgeIT.java
+++ src/test/java/io/strimzi/kafka/bridge/amqp/AmqpBridgeIT.java
@@ -129,7 +129,7 @@ class AmqpBridgeIT {
 
     @Test
     void sendSimpleMessages(VertxTestContext context) throws InterruptedException {
-        String topic = "sendSimpleMessage";
+        String topic = "sendSimpleMessages";
         KAFKA_FACADE.createTopic(topic, 1, 1);
 
         ProtonClient client = ProtonClient.create(vertx);
@@ -183,9 +183,14 @@ class AmqpBridgeIT {
 
     @Test
     void sendSimpleMessageToPartition(VertxTestContext context) throws InterruptedException {
-
         String topic = "sendSimpleMessageToPartition";
         KAFKA_FACADE.createTopic(topic, 2, 1);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         Checkpoint consume = context.checkpoint();
 
@@ -202,32 +207,28 @@ class AmqpBridgeIT {
 
                 String body = "Simple message from " + connection.getContainer();
                 Message message = ProtonHelper.message(topic, body);
-
                 Properties config = KAFKA_FACADE.getConsumerProperties();
 
                 config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
                 config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
                 KafkaConsumer<String, String> consumer = KafkaConsumer.create(this.vertx, config);
-
                 consumer.handler(record -> {
                     context.verify(() -> assertThat(record.value(), is(body)));
 //                     checking the right partition which should not be just the first one (0)
                     context.verify(() -> assertThat(record.partition(), is(1)));
-
                     LOGGER.info("Message consumed topic={} partition={} offset={}, key={}, value={}",
                             record.topic(), record.partition(), record.offset(), record.key(), record.value());
                     consumer.close();
                     consume.flag();
                 });
-
                 consumer.subscribe(topic, done -> {
                     if (!done.succeeded()) {
                         context.failNow(done.cause());
                     }
                 });
 
-//                 sending on specified partition (1)
+                // sending on specified partition (1)
                 Map<Symbol, Object> map = new HashMap<>();
                 map.put(Symbol.valueOf(AmqpBridge.AMQP_PARTITION_ANNOTATION), 1);
                 MessageAnnotations messageAnnotations = new MessageAnnotations(map);
@@ -244,6 +245,7 @@ class AmqpBridgeIT {
                 context.failNow(ar.cause());
             }
         });
+        assertThat(context.awaitCompletion(60, TimeUnit.SECONDS), is(true));
     }
 
     @Test
@@ -302,7 +304,7 @@ class AmqpBridgeIT {
                 context.failNow(ar.cause());
             }
         });
-//        assertThat(context.awaitCompletion(60, TimeUnit.SECONDS), is(true));
+        assertThat(context.awaitCompletion(60, TimeUnit.SECONDS), is(true));
     }
 
     @Test
@@ -403,9 +405,6 @@ class AmqpBridgeIT {
                 sender.send(ProtonHelper.tag("my_tag"), message, delivery -> {
                     LOGGER.info("Message delivered {}", delivery.getRemoteState());
                     context.verify(() -> assertThat(Accepted.getInstance(), is(delivery.getRemoteState())));
-
-                    sender.close();
-                    connection.close();
                 });
             } else {
                 context.failNow(ar.cause());
@@ -670,7 +669,6 @@ class AmqpBridgeIT {
         KAFKA_FACADE.produceStrings(topic, sentBody, 1, 0);
 
         ProtonClient client = ProtonClient.create(this.vertx);
-
         client.connect(AmqpBridgeIT.BRIDGE_HOST, AmqpBridgeIT.BRIDGE_PORT, ar -> {
             if (ar.succeeded()) {
 
@@ -678,12 +676,9 @@ class AmqpBridgeIT {
                 connection.open();
 
                 ProtonReceiver receiver = connection.createReceiver(topic + "/group.id/my_group");
-
                 receiver.handler((delivery, message) -> {
-                    consume.flag();
 
                     Section body = message.getBody();
-
                     if (body instanceof Data) {
                         byte[] value = ((Data) body).getValue().getArray();
 
@@ -714,7 +709,6 @@ class AmqpBridgeIT {
                     }
                 })
                     .setPrefetch(bridgeConfig.getAmqpConfig().getFlowCredit()).open();
-
             } else {
                 context.failNow(ar.cause());
             }
