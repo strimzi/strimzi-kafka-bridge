fb49664ffb2f1262afd2904642399da88bc3d39d
Jakub Scholz <www@scholzj.com>
2022-02-07 12:09:16 +0100

    Add proper dependency management to Maven build
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- src/test/java/io/strimzi/kafka/bridge/tracker/OffsetTrackerTest.java
+++ src/test/java/io/strimzi/kafka/bridge/tracker/OffsetTrackerTest.java
@@ -20,7 +20,7 @@ import java.util.Map.Entry;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class OffsetTrackerTest {
 
