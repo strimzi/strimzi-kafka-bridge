cafe7dd2c033801ed7966c82cc2d70d2591ab0e5
Paolo Patierno <ppatierno@live.com>
2023-01-05 12:07:12 +0100

    Fixed spotbugs warning
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

--- src/main/java/io/strimzi/kafka/bridge/config/ConfigRetriever.java
+++ src/main/java/io/strimzi/kafka/bridge/config/ConfigRetriever.java
@@ -6,7 +6,6 @@
 package io.strimzi.kafka.bridge.config;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
