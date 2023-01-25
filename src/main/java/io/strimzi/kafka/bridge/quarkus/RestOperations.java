35089e15f705869d34f094863bb529f8f75c6b11
Paolo Patierno <ppatierno@live.com>
2023-01-25 17:23:27 +0100

    Added RestUtils class to build HTTP response Handling producer disabled in the send operation
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

--- src/main/java/io/strimzi/kafka/bridge/quarkus/RestOperations.java
+++ src/main/java/io/strimzi/kafka/bridge/quarkus/RestOperations.java
@@ -5,22 +5,70 @@
 
 package io.strimzi.kafka.bridge.quarkus;
 
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.strimzi.kafka.bridge.BridgeContentType;
+import io.strimzi.kafka.bridge.http.HttpBridgeContext;
+import io.strimzi.kafka.bridge.http.converter.JsonUtils;
+import io.strimzi.kafka.bridge.http.model.HttpBridgeError;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.ext.web.RoutingContext;
+import org.jboss.logging.Logger;
+
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
 @Path("/")
 public class RestOperations {
 
     @Inject
+    Logger log;
+
+    @Inject
     BridgeConfigRetriever configRetriever;
 
+    private HttpBridgeContext<byte[], byte[]> httpBridgeContext;
+
+    @PostConstruct
+    public void init() {
+        httpBridgeContext = new HttpBridgeContext<>();
+    }
+
     @Path("/topics/{topicname}")
-    @GET
-    public Response send(@PathParam("topicname") String topicName, @QueryParam("async") boolean async) {
-        return Response.ok(topicName).build();
+    @POST
+    public CompletionStage<Response> send(@Context RoutingContext routingContext, @PathParam("topicname") String topicName, @QueryParam("async") boolean async) {
+        log.infof("send thread %s", Thread.currentThread());
+        return processProducer(routingContext);
+    }
+
+    /**
+     * Process an HTTP request related to the producer
+     *
+     * @param routingContext RoutingContext instance
+     */
+    private CompletionStage<Response> processProducer(RoutingContext routingContext) {
+        if (!configRetriever.config().getHttpConfig().isProducerEnabled()) {
+            HttpBridgeError error = new HttpBridgeError(
+                    HttpResponseStatus.SERVICE_UNAVAILABLE.code(),
+                    "Producer is disabled in config. To enable producer update http.producer.enabled to true"
+            );
+            Response response = RestUtils.buildResponse(HttpResponseStatus.SERVICE_UNAVAILABLE.code(),
+                    BridgeContentType.KAFKA_JSON, JsonUtils.jsonToBuffer(error.toJson()));
+            return CompletableFuture.completedStage(response);
+        }
+        HttpServerRequest httpServerRequest = routingContext.request();
+        String contentType = httpServerRequest.getHeader("Content-Type") != null ?
+                httpServerRequest.getHeader("Content-Type") : BridgeContentType.KAFKA_JSON_BINARY;
+
+        // TODO
+
+        return CompletableFuture.completedStage(Response.ok("processProducer").build());
     }
 }
---
+++ src/main/java/io/strimzi/kafka/bridge/quarkus/RestUtils.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+
+package io.strimzi.kafka.bridge.quarkus;
+
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.strimzi.kafka.bridge.http.converter.JsonUtils;
+import io.vertx.core.buffer.Buffer;
+import org.jboss.logging.Logger;
+
+import javax.ws.rs.core.Response;
+
+/**
+ * Provides some utility methods for HTTP request/response
+ */
+public class RestUtils {
+
+    private static final Logger log = Logger.getLogger(RestUtils.class);
+
+    /**
+     * Build an HTTP response
+     *
+     * @param statusCode the HTTP status code
+     * @param contentType the content-type to set in the HTTP response
+     * @param body the body to set in the HTTP response
+     */
+    public static Response buildResponse(int statusCode, String contentType, Buffer body) {
+        Response.ResponseBuilder builder = Response.status(statusCode);
+        if (body != null) {
+            log.debugf("Response: body = %s", JsonUtils.bufferToJson(body));
+            builder.header(HttpHeaderNames.CONTENT_TYPE.toString(), contentType);
+            builder.header(HttpHeaderNames.CONTENT_LENGTH.toString(), String.valueOf(body.length()));
+            builder.entity(body);
+        }
+        return builder.build();
+    }
+}
