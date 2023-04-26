8902796657a743544d2504f84c593e83f7cca6b0
Shubham Rawat <40595489+ShubhamRwt@users.noreply.github.com>
2023-04-26 21:30:20 +0530

    Add Http related and Kafka related metrics to the Bridge (#7)
    * Added metrics as per Erin's suggestion
    
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>
    
    * Fixes
    
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>
    
    * Reverted deletion of some deps
    
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>
    
    * Fixed Tags
    
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>
    
    * Changed the tag names
    
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>
    
    * suppress /metrics
    
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>
    
    * Added suggestions by Paolo
    
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>
    
    * Added suggestions by Paolo
    
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>
    
    * Revert to 8080
    
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>
    
    ---------
    
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>

---
+++ src/main/java/io/strimzi/kafka/bridge/quarkus/RestBridgeMeterRegistryProducer.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+
+package io.strimzi.kafka.bridge.quarkus;
+
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.micrometer.prometheus.PrometheusNamingConvention;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * Provides a custom Prometheus registry able to host both Quarkus built-in HTTP related metrics
+ * and Kafka clients related metrics coming through JMX collector
+ */
+@Singleton
+public class RestBridgeMeterRegistryProducer extends PrometheusMeterRegistry {
+
+    @ConfigProperty(name = "kafka.bridge.metrics.enabled", defaultValue = "false")
+    boolean isMetricsEnabled;
+
+    @Inject
+    JmxCollectorRegistry restJmxCollectorRegistry;
+
+    @PostConstruct
+    void init() {
+        this.config().meterFilter(
+                MeterFilter.deny(meter -> "/metrics".equals(meter.getTag("uri"))));
+        this.config().namingConvention(new RestBridgeMeterRegistryProducer.MetricsNamingConvention());
+    }
+
+    /**
+     * Came in as part of extending `PrometheusMeterRegistry since
+     * it has no default constructors available
+     */
+    private RestBridgeMeterRegistryProducer(PrometheusConfig config) {
+        super(config);
+    }
+
+    @Produces
+    @Singleton
+    RestBridgeMeterRegistryProducer createPrometheusMeterRegistry() {
+        return this;
+    }
+
+    /**
+     * Scrape Quarkus built-in meter registry for HTTP related metrics and
+     * JMX collector registry for Kafka clients related metrics
+     *
+     * @return metrics in Prometheus format as String
+     */
+    @Override
+    public String scrape() {
+        StringBuilder sb = new StringBuilder();
+        if (isMetricsEnabled) {
+            if (restJmxCollectorRegistry != null) {
+                sb.append(restJmxCollectorRegistry.scrape());
+            }
+            sb.append(super.scrape());
+        }
+        return sb.toString();
+    }
+
+    private static class MetricsNamingConvention extends PrometheusNamingConvention {
+        @Override
+        public String name(String name, Meter.Type type, String baseUnit) {
+            String metricName = name.startsWith("http.") ? "strimzi.bridge." + name : name;
+            return super.name(metricName, type, baseUnit);
+        }
+
+        @Override
+        public String tagKey(String key) {
+            String tag = key.replace("uri", "path").replace("status", "code");
+            return super.tagKey(tag);
+        }
+    }
+}
+
