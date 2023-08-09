5e062577f4adc2aba522d37e5d9606febbc48931
Paolo Patierno <ppatierno@live.com>
2023-08-09 15:38:28 +0200

    Hiding of TLS related password when logging configuration
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

--- src/test/java/io/strimzi/kafka/bridge/config/ConfigRetrieverTest.java
+++ src/test/java/io/strimzi/kafka/bridge/config/ConfigRetrieverTest.java
@@ -3,7 +3,7 @@
  * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
  */
 
-package io.strimzi.kafka.bridge;
+package io.strimzi.kafka.bridge.config;
 
 import io.strimzi.kafka.bridge.config.BridgeConfig;
 import io.strimzi.kafka.bridge.config.ConfigRetriever;
