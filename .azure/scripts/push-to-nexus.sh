65655677e86118d378e0ebad65ecc77a0161451c
Jakub Scholz <www@scholzj.com>
2024-06-30 16:51:24 +0200

    Fix pushing of artifacts to Nexus
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- .azure/scripts/push-to-nexus.sh
+++ .azure/scripts/push-to-nexus.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 
+set -e
+
 echo "Build reason: ${BUILD_REASON}"
 echo "Source branch: ${BRANCH}"
 
-GPG_TTY=$(tty)
-export GPG_TTY
+function cleanup() {
+  rm -rf signing.gpg
+  gpg --delete-keys
+  gpg --delete-secret-keys
+}
 
-echo "$GPG_SIGNING_KEY" | base64 -d > signing.gpg
+# Run the cleanup on failure / exit
+trap cleanup EXIT
+
+export GPG_TTY=$(tty)
+echo $GPG_SIGNING_KEY | base64 -d > signing.gpg
 gpg --batch --import signing.gpg
 
 GPG_EXECUTABLE=gpg mvn $MVN_ARGS -DskipTests -s ./.azure/scripts/settings.xml -P ossrh verify deploy
 
-rm -rf signing.gpg
-gpg --delete-keys
-gpg --delete-secret-keys
+cleanup
