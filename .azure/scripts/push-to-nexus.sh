c9de8e4434b1ba031297e06a208e67f2949b9670
Paolo Patierno <ppatierno@live.com>
2024-12-20 13:42:40 +0100

    Try fixing push Nexus with Java 17
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

--- .azure/scripts/push-to-nexus.sh
+++ .azure/scripts/push-to-nexus.sh
@@ -18,6 +18,8 @@ export GPG_TTY=$(tty)
 echo $GPG_SIGNING_KEY | base64 -d > signing.gpg
 gpg --batch --import signing.gpg
 
-GPG_EXECUTABLE=gpg mvn $MVN_ARGS -DskipTests -s ./.azure/scripts/settings.xml -P ossrh verify deploy
+#GPG_EXECUTABLE=gpg mvn $MVN_ARGS -DskipTests -s ./.azure/scripts/settings.xml -P ossrh verify deploy
+echo "JAVA_HOME: " ${JAVA_HOME}
+mvn -version
 
 cleanup
