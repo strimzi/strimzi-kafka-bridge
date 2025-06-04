e0222fbc7a00ed35e7d8861a4a149ec8bef7649a
see-quick <maros.orsak159@gmail.com>
2025-06-04 11:59:08 +0200

    checkstyle
    Signed-off-by: see-quick <maros.orsak159@gmail.com>

--- src/test/java/io/strimzi/kafka/bridge/http/base/HttpBridgeITAbstract.java
+++ src/test/java/io/strimzi/kafka/bridge/http/base/HttpBridgeITAbstract.java
@@ -34,8 +34,6 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
